3001c2ee26703308c2dab14fd6fd301a180bb4f1
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-04-19 20:40:10 -0400

    Delete stuff

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Control.Monad
-import Control.Monad.IO.Class
-import Data.IORef
-import Graphics.UI.Gtk hiding (Action, backspace)
-
-
-main :: IO ()
-main = do
-  void initGUI          -- (1)
-  window <- windowNew   -- (2)
-                        -- (3)
-  widgetShowAll window  -- (4)
-  mainGUI               -- (5)
+import Reflex.Dom
+main = mainWidget $ text "Hello, world!"
