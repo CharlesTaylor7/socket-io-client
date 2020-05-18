328eb0f0b0d0dcb8bcbab274630c54b8648039b9
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-05-18 17:38:45 -0400

    cache lib.js

--- build-hs.sh
+++ build-hs.sh
@@ -4,6 +4,6 @@ if cabal build generals-app; then
   public_build="../public/js/ghcjs/build"
   build_output="dist-newstyle/build/x86_64-linux/ghcjs-8.6.0.1/generals-0.0.0.0/x/generals-app/build/generals-app/generals-app.jsexe"
   mkdir -p "$public_build"
-  cp "$build_output"/lib.js "$public_build"
+  # cp "$build_output"/lib.js "$public_build"
   cp "$build_output"/out.js "$public_build"
 fi
