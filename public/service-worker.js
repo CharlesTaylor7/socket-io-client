77906c971fec58fcc6973c28e789b64d487aa3e2
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-05-23 01:29:54 -0400

    Remove logs

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,18 +13,14 @@ self.addEventListener('install', event => {
 const replayStorageRegex = /https:\/\/generalsio-replays-(na|bot)\.s3\.amazonaws\.com\/\w+\.gior/
 
 self.addEventListener('fetch', event => {
-  console.log(event.request.url);
   if (replayStorageRegex.test(event.request.url)) {
-    console.log("looking in replays cache")
     event.respondWith(
       (async function() {
         const replaysCache = await caches.open('replays')
         let response = await replaysCache.match(event.request)
         if (response) {
-          console.log("returning from cache")
           return response
         }
-        console.log("performing request from cache")
         response = await fetch(event.request)
         await replaysCache.put(event.request, response);
         return response
