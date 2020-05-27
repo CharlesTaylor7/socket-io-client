d325643e2b8b747c333aab6625e974ea37b01d8e
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-05-27 16:22:33 -0400

    Update _Army traversal

--- hs/app/Generals/Map/Types/Optics.hs
+++ hs/app/Generals/Map/Types/Optics.hs
@@ -4,7 +4,7 @@ import Generals.Map.Types.Definitions
 
 
 _Army :: Traversal' Tile Army
-_Army = _Clear `failing` _City `failing` _General
+_Army = _Clear `failing` _City `failing` _General `failing` _Swamp
 
 _Owner :: Traversal' Tile Owner
 _Owner = _Army . owner
