0265fb553bb9425e3cc282b14aa7a9ce4ba01b0e
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-05-30 21:33:28 -0400

    Works

--- hs/app/Component/Elastic.hs
+++ hs/app/Component/Elastic.hs
@@ -35,63 +35,83 @@ elastic dims child = do
       )
   rec
     (e, a) <- elStyle' "div" elasticStyle $ child dynChildStyle
-
-    mousePosition <- holdDyn zero mouseMove
-
-    dragReferencePoint <- accumB (&) zero $
-      leftmost $
-        [ mouseDown & fmapCheap add
-        , mouseUp & fmapCheap subtract
-        -- , dynScale & updated &
-        ]
-
-    draggingBehavior <- hold DragOff toggleDragEvent
-
-    dragAmount :: Dynamic t (Point Int) <- holdDyn zero $ dragEvent
-  -- range from quarter size to 4 times as big
-    zoomLevel <- accumDyn (\a b -> a + b & clamp (-750) 3000) 0 $ wheelEvent
-
-    display mousePosition
-    let
-      dynScale = zoomLevel <&> \level -> 1 + 0.001 * level
-
-      dynChildStyle :: Dynamic t Style
-      dynChildStyle = zipDynWith combine dragAmount dynScale
-
-      toDoublePoint :: Point Int -> Point Double
-      toDoublePoint = view $ coerced . converted . coerced
-        where
-          converted = alongside (to int2Double) (to int2Double)
-          int2Double :: Int -> Double
-          int2Double = fromIntegral
-
-      combine :: Point Int -> Double -> Style
-      combine offset zoom = toStyle dims $ Transform (toDoublePoint offset) zoom
-
-      wheelEvent :: Event t Double
-      wheelEvent = domEvent Wheel e & fmapCheap _wheelEventResult_deltaY
-
-      dragEvent :: Event t (Point Int)
-      dragEvent = mouseMove
-        & gate (coerceBehavior draggingBehavior)
-        & attachWith subtract dragReferencePoint
-
-      mouseMove, mouseDown, mouseUp :: Event t (Point Int)
-      mouseMove = mouseEvent Mousemove e
-      mouseDown = mouseEvent Mousedown e
-      mouseUp   = mouseEvent Mouseup   e
-
-      mouseLeave :: Event t ()
-      mouseLeave = domEvent Mouseleave e
-
-      toggleDragEvent :: Event t Dragging
-      toggleDragEvent = fold
-        [ mouseDown  $> DragOn
-        , mouseUp    $> DragOff
-        , mouseLeave $> DragOff
-        ]
+    dynChildStyle <- getTransform e <&> fmap (toStyle dims)
 
   pure a
+
+eitherOr :: Show a => These a a -> Maybe a
+eitherOr (This a) = Just a
+eitherOr (That a) = Just a
+eitherOr (These a b) = Nothing & trace ("Coincidence:" <> show (a, b))
+
+
+getTransform :: forall t m. Widget t m => Element t m -> m (Dynamic t Transform)
+getTransform e = do
+  let
+    toggleDragEvent :: Event t Dragging
+    toggleDragEvent = fold
+      [ mouseDown  $> DragOn
+      , mouseUp    $> DragOff
+      , mouseLeave $> DragOff
+      ]
+  dragging :: Behavior t Dragging <- hold DragOff toggleDragEvent
+
+  mousePositionDyn <- holdDyn zero mouseMove
+  -- temporary
+  let mousePosition = current mousePositionDyn
+
+  dragReferencePointDyn :: Dynamic t (Point Int) <-
+    accumDyn (&) zero $ leftmost $
+      [ mouseDown  & fmapCheap add
+      , mouseUp    & fmapCheap subtract
+      , mouseLeave
+        & gate (coerceBehavior dragging)
+        & attachWith (const . subtract) mousePosition
+      ]
+  -- temporary
+  let dragReferencePoint = current dragReferencePointDyn
+
+
+  let
+    dragEvent :: Event t (Point Int)
+    dragEvent = mouseMove
+      & gate (coerceBehavior dragging)
+      & attachWith subtract dragReferencePoint
+
+
+  dragAmount :: Dynamic t (Point Int) <- holdDyn zero $ dragEvent
+-- range from quarter size to 4 times as big
+  zoomLevel :: Dynamic t Double <- accumDyn (\a b -> a + b & clamp (-750) 3000) 0 $ wheelEvent
+
+  let
+    dynScale = zoomLevel <&> \level -> 1 + 0.001 * level
+
+    dynTransform :: Dynamic t Transform
+    dynTransform = zipDynWith combine dragAmount dynScale
+
+    toDoublePoint :: Point Int -> Point Double
+    toDoublePoint = view $ coerced . converted . coerced
+      where
+        converted = alongside (to int2Double) (to int2Double)
+        int2Double :: Int -> Double
+        int2Double = fromIntegral
+
+    combine :: Point Int -> Double -> Transform
+    combine offset zoom = Transform (toDoublePoint offset) zoom
+
+  pure dynTransform
+  where
+    wheelEvent :: Event t Double
+    wheelEvent = domEvent Wheel e & fmapCheap _wheelEventResult_deltaY
+
+    mouseMove, mouseDown, mouseUp :: Event t (Point Int)
+    mouseMove = mouseEvent Mousemove e
+    mouseDown = mouseEvent Mousedown e
+    mouseUp   = mouseEvent Mouseup   e
+
+    mouseLeave :: Event t ()
+    mouseLeave = domEvent Mouseleave e
+
 
 clamp :: Ord n => n -> n -> n -> n
 clamp min max n
