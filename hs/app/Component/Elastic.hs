843edbb17885327b29232be2bc33e9c95d639554
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-05-31 00:28:10 -0400

    Simplify

--- hs/app/Component/Elastic.hs
+++ hs/app/Component/Elastic.hs
@@ -46,18 +46,22 @@ elastic dims child = do
 
 getTransform :: forall t m. Widget t m => Element t m -> m (Dynamic t Transform)
 getTransform e = do
-  let
-    toggleDragEvent :: Event t Dragging
-    toggleDragEvent = fold
+  dragging :: Behavior t Dragging <-
+    hold DragOff $ fold
       [ mouseDown  $> DragOn
       , mouseUp    $> DragOff
       , mouseLeave $> DragOff
       ]
-  dragging :: Behavior t Dragging <- hold DragOff toggleDragEvent
 
-  mousePosition :: Behavior t Point <- hold zero mouseMove
+  mousePosition :: Behavior t Point <-
+    hold zero mouseMove
 
-  zoomLevel :: Dynamic t Double <- accumDyn zoomReducer 0 $ wheelEvent
+  zoomScale :: Dynamic t Double <-
+    wheelEvent
+    & accumDyn zoomReducer 0
+    -- range from quarter size to 4 times as big
+    <&> fmap (\level -> 1 + 0.001 * level)
+
 
   dragReferencePointDyn :: Dynamic t Point <-
     accumDyn (&) zero $ leftmost
@@ -74,27 +78,26 @@ getTransform e = do
         & gate (coerceBehavior dragging)
         & traceEventWith (\p -> "MouseLeave: " <> show p)
         & attachWith (const . add) mousePosition
-
-      -- , zoomLevel
-      --   & updated
-      --   & traceEventWith (\zoom -> "Zoom: " <> show zoom)
-      --   & attachWith (\mouse zoom -> add mouse . scale zoom . subtract mouse) mousePosition
       ]
   let dragReferencePoint = current dragReferencePointDyn
 
   display dragReferencePointDyn
 
-  dragAmount :: Dynamic t Point <- holdDyn zero $
-    mouseMove
-    & gate (coerceBehavior dragging)
-    & attachWith add dragReferencePoint
+  dynOffset :: Dynamic t Point <-
+    holdDyn zero $ leftmost
+      [ mouseMove
+        & gate (coerceBehavior dragging)
+        & attachWith add dragReferencePoint
+      , zoomScale
+        & updated
+        & traceEventWith (\zoom -> "Zoom: " <> show zoom)
+        <&> \(!_) -> mempty
+        -- & attachWith (\mouse zoom -> add mouse . scale zoom . subtract mouse) mousePosition
+      ]
 
   let
-    -- range from quarter size to 4 times as big
-    dynScale = zoomLevel <&> \level -> 1 + 0.001 * level
-
     dynTransform :: Dynamic t Transform
-    dynTransform = zipDynWith Transform dragAmount dynScale
+    dynTransform = zipDynWith Transform dynOffset zoomScale
 
   pure dynTransform
   where
