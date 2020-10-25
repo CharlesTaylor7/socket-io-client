cccd5a8cd2d669de618b28fcf75390194a947a24
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-10-24 22:16:52 -0400

    Delete reflex-imports

--- hs/reflex-imports/Reflex/Instances.hs
+++
@@ -1,7 +0,0 @@
-module Reflex.Instances where
-
-import Reflex.Dom (Event, Reflex, never)
-import Data.Default
-
-instance Reflex t => Default (Event t m) where
-  def = never
--- hs/reflex-imports/Reflex/Optics.hs
+++
@@ -1,7 +0,0 @@
-{-# language TemplateHaskell #-}
-module Reflex.Optics where
-
-import Reflex.Dom (InputElement(..))
-import Control.Lens.TH (makeLenses)
-
-makeLenses ''InputElement
