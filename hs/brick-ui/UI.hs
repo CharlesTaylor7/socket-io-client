a9817f7e59dbbdb43611c3f7b0352f0051cd7224
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-10-25 15:03:13 -0400

    Start updating to use brick-grid

--- hs/brick-ui/UI.hs
+++ hs/brick-ui/UI.hs
@@ -9,11 +9,14 @@ import qualified Brick as Scroll (ViewportType(..))
 import qualified Brick as Brick
 
 import Brick.BChan (newBChan, writeBChan)
--- import Brick.Widgets.Border
 import Brick.Widgets.Border.Style
 import Brick.Widgets.Center
 
+import Brick.Grid (GridStyle(..))
+import qualified Brick.Grid as Grid
+
 import qualified Graphics.Vty as V
+
 
 -- | show army count in 4 characters
 -- if the army is over 4 digits, use k prefix
@@ -83,13 +86,6 @@ data Name = GridView
   deriving (Eq, Ord, Show)
 
 type Widget = Brick.Widget Name
-
-data Cell
-  = Snake
-  | Food
-  | Empty
-  deriving (Eq, Show)
-
 type AppState = (History, TurnIndex)
 
 -- App definition
@@ -134,62 +130,27 @@ drawUI (history, TurnIndex turn) =
   [ center $ flip runReader gridStyle $ drawGrid game
   ]
   where
+    game :: GameInfo
     game = history ^?! ix turn $ "history index"
+
+    toTile :: (Int, Int) -> Tile
+    toTile (i, j) = game ^?! #grid . ixGrid (j * width + i) $ "grid index"
+
+    width = game ^. #replay . #mapWidth
+    height = game ^. #replay . #mapHeight
+
     gridStyle = GridStyle
       { borderStyle = unicode
-      , cellSize = 4
-      , gridWidth = game ^. #replay . #mapWidth
-      , gridHeight = game ^. #replay . #mapHeight
+      , cellWidth = 4
+      , gridWidth = width
+      , gridHeight = height
+      , drawTileWith = flip runReader gridStyle . drawTile . toTile
       }
-data GridStyle = GridStyle
-  { borderStyle :: BorderStyle
-  , cellSize :: Int
-  , gridWidth :: Int
-  , gridHeight :: Int
-  }
-  deriving (Generic)
-
-
-drawTitle :: GameInfo -> Widget
-drawTitle gameInfo = do
-  let
-    dimensions = show width <> "x" <> show height
-    width = gameInfo ^. #replay . #mapWidth
-    height = gameInfo ^. #replay . #mapHeight
-
-  hCenter $ str $ "Replay " <> dimensions
-
-
-drawGrid :: MonadReader GridStyle m => GameInfo -> m Widget
-drawGrid gameInfo = do
-  let
-    width = gameInfo ^. #replay . #mapWidth
-    height = gameInfo ^. #replay . #mapHeight
-
-  rows <-
-    for [1..height] $ \y -> do
-      row <- for [1..width] $ \x -> do
-        let
-          tile = gameInfo ^. #grid . ixGrid i
-          i = GridIndex $ (y - 1) * width + (x - 1)
-        drawTile tile
-      insertVBorders row
-
-  gridContent <- insertHBorders rows
-
-  let
-    grid :: Widget
-    grid =
-      viewport GridView Scroll.Both $
-      cached GridView $
-        gridContent
-
-  pure $ drawTitle gameInfo <=> grid
 
 -- | Draw tile
 drawTile :: MonadReader GridStyle m => Tile -> m Widget
 drawTile tile = do
-  cellWidth <- view #cellSize
+  cellWidth <- view #cellWidth
   pure
     $ str (tile ^. contents cellWidth)
     & withAttr (tile ^. ownerAttr <> tile ^. to terrainAttr)
@@ -205,6 +166,28 @@ drawTile tile = do
       `failing`
       like (replicate w ' ')
 
+
+drawTitle :: GameInfo -> Widget
+drawTitle gameInfo = do
+  let
+    dimensions = show width <> "x" <> show height
+    width = gameInfo ^. #replay . #mapWidth
+    height = gameInfo ^. #replay . #mapHeight
+
+  hCenter $ str $ "Replay " <> dimensions
+
+
+drawGrid :: MonadReader GridStyle m => GameInfo -> m Widget
+drawGrid gameInfo = do
+  gridContent <- reader Grid.drawGrid
+  let
+    grid =
+      viewport GridView Scroll.Both $
+      cached GridView $
+        gridContent
+
+  pure $ drawTitle gameInfo <=> grid
+
 terrainAttr :: Tile -> AttrName
 terrainAttr (Clear _) = "clear"
 terrainAttr (City _) = "city"
@@ -213,46 +196,3 @@ terrainAttr (Swamp _) = "swamp"
 terrainAttr Mountain  = "mountain"
 terrainAttr Fog_Clear  = "fog"
 terrainAttr Fog_Obstacle  = "fog"
-
-insertVBorders :: MonadReader GridStyle m => [Widget] -> m Widget
-insertVBorders cells = do
-  v <- view $ #borderStyle . #bsVertical . to (str . pure)
-  pure . hBox . (v:) . (<> [v]) . intersperse v $ cells
-
-insertHBorders :: MonadReader GridStyle m => [Widget] -> m Widget
-insertHBorders cells = do
-  h1 <- hBorder Top
-  h2 <- hBorder Bottom
-  h3 <- hBorder Middle
-  pure . vBox . (h1 :) . (<> [h2]) . intersperse h3 $ cells
-
-
-hBorder :: MonadReader GridStyle m => VLocation -> m Widget
-hBorder v = do
-  cellWidth <- view #cellSize
-  mapWidth <- view #gridWidth
-  innerBorder <- view $ #borderStyle . borderStyleL v Center
-  startCorner <- view $ #borderStyle . borderStyleL v Start
-  endCorner <- view $ #borderStyle . borderStyleL v End
-  pipe <- view $ #borderStyle . #bsHorizontal
-  replicate cellWidth pipe
-      & replicate mapWidth
-      & intercalate [innerBorder]
-      & \row -> [startCorner] <> row <> [endCorner]
-      & str
-      & pure
-
-data VLocation = Bottom | Middle | Top
-data HLocation = Start | Center | End
-data Pipe = Horizontal | Vertical
-
-borderStyleL :: VLocation -> HLocation -> Lens' BorderStyle Char
-borderStyleL Bottom Start  = #bsCornerBL
-borderStyleL Bottom Center = #bsIntersectB
-borderStyleL Bottom End    = #bsCornerBR
-borderStyleL Middle Start  = #bsIntersectL
-borderStyleL Middle Center = #bsIntersectFull
-borderStyleL Middle End    = #bsIntersectR
-borderStyleL Top Start     = #bsCornerTL
-borderStyleL Top Center    = #bsIntersectT
-borderStyleL Top End       = #bsCornerTR
