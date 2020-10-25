8549a02f34d311dc298815b9284d084ee2e01c6c
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-10-25 17:25:50 -0400

    Update loadReplay

--- hs/lib/Node/FFI.hs
+++ hs/lib/Node/FFI.hs
@@ -1,12 +1,34 @@
 module Node.FFI (loadReplay) where
 
-import System.Process
-
 import Types
 import Generals.Replay.Decode
 
+import System.IO
+import qualified Data.Bytestring as BS
+import qualified Data.ByteString.Base64 as Base64
+import qualified LZString as LZ
 
 newtype Url = Url Text
+
+loadReplay :: ReplayLocation -> IO Replay
+loadReplay location = do
+  let Url url = replayUrl location
+  case location of
+    Server_Local -> do
+      handle <- openFile url ReadMode
+      contents <- Base64 <$> BS.hGetContents handle
+      pure $ LZ.decompressBase64 contents
+
+    _ -> undefined
+
+  (exitCode, stdOut, stdErr) <-
+    readProcessWithExitCode "node" ["js/download-replay.js", url ^. unpacked] ""
+
+  putStrLn stdErr
+
+  pure $ decode $ stdOut ^. packed
+
+
 
 
 replayDir :: Server -> Text
@@ -28,14 +50,3 @@ replayUrl replay = Url
   $  replay ^. #server . to replayDir
   <> replay ^. #id . to replayFile
 
-
-loadReplay :: ReplayLocation -> IO Replay
-loadReplay location = do
-  let Url url = replayUrl location
-
-  (exitCode, stdOut, stdErr) <-
-    readProcessWithExitCode "node" ["js/download-replay.js", url ^. unpacked] ""
-
-  putStrLn stdErr
-
-  pure $ decode $ stdOut ^. packed
