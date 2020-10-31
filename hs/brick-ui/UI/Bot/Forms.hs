d1a64a232a640d5bc480b4d8d8bc0a7ac815d560
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-10-31 15:22:26 -0400

    work

--- hs/brick-ui/UI/Bot/Forms.hs
+++ hs/brick-ui/UI/Bot/Forms.hs
@@ -1,14 +1,1 @@
 module UI.Bot.Forms where
-
-import Generals.Types
-import Brick.Forms (Form, newForm, editShowableField, setFormFocus)
-
-import UI.Bot.Types
-
-
-newJumpToTurnForm :: TurnIndex -> Form TurnIndex e Name
-newJumpToTurnForm =
-  setFormFocus JumpToTurnInput .
-  newForm
-    [ editShowableField _TurnIndex JumpToTurnInput
-    ]
