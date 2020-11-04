6405a172f64191b2af9e1aa95b9b3d08779935a4
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-11-03 22:15:43 -0500

    Event GADT

--- generals-io-client/GeneralsIO/Protocol/Events.hs
+++ generals-io-client/GeneralsIO/Protocol/Events.hs
@@ -7,20 +7,27 @@ module GeneralsIO.Protocol.Events
   ) where
 -- import Pipes (Pipe)
 import qualified GeneralsIO.Events as Evt
-import GeneralsIO.Protocol.Phases
+import GeneralsIO.Protocol.Phases as Phase
 
 
 -- | Event GADT
 -- Event triggers the current next change
 data Event (current :: Phase) (next :: Phase) where
-  QueueUpdate      :: Evt.QueueUpdate      -> Event current next
-  ChatMessageE     :: ChatMessageEvent current next => Evt.ChatMessage      -> Event current next
-  Notify           :: Evt.Notify           -> Event current next -- any
-  PreGameStart     :: Evt.PreGameStart     -> Event current next -- ?
-  GameUpdate       :: Evt.GameUpdate       -> Event (InGame g) (InGame g)
-  ErrorSetUsername :: Evt.ErrorSetUsername -> Event current next -- ?
+  QueueUpdate      :: QueueUpdateEvent current next => Evt.QueueUpdate      -> Event current     next
+  ChatMessageE     :: ChatMessageEvent current next => Evt.ChatMessage      -> Event current     next
+  Notify           ::                                  Evt.Notify           -> Event current     next -- any
+  PreGameStart     ::                                  Evt.PreGameStart     -> Event current     next -- ?
+  GameStart        ::                                  Evt.GameStart        -> Event (InQueue g) (InGame g)
+  GameUpdate       ::                                  Evt.GameUpdate       -> Event (InGame g)  (InGame g)
+  GameOver         ::                                  Evt.GameOver         -> Event (InGame g)  Phase.GameOver
+  ErrorSetUsername ::                                  Evt.ErrorSetUsername -> Event current     next -- ?
 
 
 class ChatMessageEvent (current :: Phase) (next :: Phase)
 instance ChatMessageEvent (InQueue g) (InQueue g)
 instance ChatMessageEvent (InGame g)  (InGame g)
+
+
+class QueueUpdateEvent (current :: Phase) (next :: Phase)
+instance QueueUpdateEvent Connected   (InQueue g)
+instance QueueUpdateEvent (InQueue g) (InQueue g)
