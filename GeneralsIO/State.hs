767f3e6168fb74d26e110ec28d3944f137045fe7
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-11-03 23:37:28 -0500

    Work

--- GeneralsIO/State.hs
+++ GeneralsIO/State.hs
@@ -2,3 +2,12 @@ module GeneralsIO.State
   (
   )
   where
+
+
+type GridIndex = (Int, Int)
+type PlayerId = Int
+
+data GameState = GameState
+  { grid     :: Map GridIndex Tile
+  , generals :: Map GridIndex PlayerId
+  }
