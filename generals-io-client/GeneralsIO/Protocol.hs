7e98c94558d74fbef70cecd37eadfd7e897ad9a1
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-11-03 21:02:07 -0500

    Stab at tying gametypes to only some phases

--- generals-io-client/GeneralsIO/Protocol.hs
+++ generals-io-client/GeneralsIO/Protocol.hs
@@ -3,7 +3,7 @@
 {-# Language TypeFamilies #-}
 module GeneralsIO.Protocol where
 -- import Pipes (Pipe)
-import GeneralsIO.Events (Event)
+-- import GeneralsIO.Events (Event)
 import qualified GeneralsIO.Commands as Cmd
 
 
@@ -16,30 +16,30 @@ data GameType
 
 
 data Phase
-  = Connected -- ^ connected to the server
-  | InQueue   -- ^ in a queue
-  | InGame    -- ^ in a game
-  | GameOver  -- ^ game has ended
+  = Connected        -- ^ connected to the server
+  | InQueue GameType -- ^ in a queue
+  | InGame  GameType -- ^ in a game
+  | GameOver         -- ^ game has ended
 
 
-data Command (phase :: Phase) (gameType :: GameType) where
-  SetUsername   ::   {- ?? -}                   Cmd.SetUsername   -> Command phase     gameType
-  StarsAndRank  ::   {- ?? -}                   Cmd.StarsAndRank  -> Command phase     gameType
-  Play          ::                              Cmd.Play          -> Command Connected gameType
-  Join1v1       ::                              Cmd.Join1v1       -> Command Connected gameType
-  SetCustomTeam ::                              Cmd.SetCustomTeam -> Command InQueue   Custom
-  JoinTeam      ::                              Cmd.JoinTeam      -> Command InQueue   TwoVsTwo
-  LeaveTeam     ::                              Cmd.LeaveTeam     -> Command InQueue   TwoVsTwo
-  Cancel        ::                              Cmd.Cancel        -> Command InQueue   gameType
-  SetForceStart :: CanSetForceStart gameType => Cmd.SetForceStart -> Command InQueue   gameType
-  Attack        ::                              Cmd.Attack        -> Command InGame    gameType
-  ClearMoves    ::                              Cmd.ClearMoves    -> Command InGame    gameType
-  PingTile      :: IsTeamGame gameType       => Cmd.PingTile      -> Command InGame    gameType
-  ChatMessage   ::                              Cmd.ChatMessage   -> Command phase     gameType
-  LeaveGame     :: CanLeaveGame phase        => Cmd.LeaveGame     -> Command phase     gameType
+data Command (phase :: Phase)  where
+  SetUsername   ::   {- ?? -}                   Cmd.SetUsername   -> Command phase
+  StarsAndRank  ::   {- ?? -}                   Cmd.StarsAndRank  -> Command phase
+  Play          ::                              Cmd.Play          -> Command Connected
+  Join1v1       ::                              Cmd.Join1v1       -> Command Connected
+  SetCustomTeam ::                              Cmd.SetCustomTeam -> Command (InQueue   Custom)
+  JoinTeam      ::                              Cmd.JoinTeam      -> Command (InQueue   TwoVsTwo)
+  LeaveTeam     ::                              Cmd.LeaveTeam     -> Command (InQueue   TwoVsTwo)
+  Cancel        ::                              Cmd.Cancel        -> Command (InQueue   gameType)
+  SetForceStart :: CanSetForceStart gameType => Cmd.SetForceStart -> Command (InQueue   gameType)
+  Attack        ::                              Cmd.Attack        -> Command (InGame    gameType)
+  ClearMoves    ::                              Cmd.ClearMoves    -> Command (InGame    gameType)
+  PingTile      :: IsTeamGame gameType       => Cmd.PingTile      -> Command (InGame    gameType)
+  ChatMessage   :: CanChat phase             => Cmd.ChatMessage   -> Command phase
+  LeaveGame     :: CanLeaveGame phase        => Cmd.LeaveGame     -> Command phase
 
 class CanLeaveGame (phase :: Phase)
-instance CanLeaveGame InGame
+instance CanLeaveGame (InGame g)
 instance CanLeaveGame GameOver
 
 class IsTeamGame (gameType :: GameType)
@@ -50,3 +50,12 @@ instance IsTeamGame FFA
 class CanSetForceStart (gameType :: GameType)
 instance CanSetForceStart Custom
 instance CanSetForceStart FFA
+
+class CanChat (phase :: Phase)
+instance CanChat (InQueue g)
+instance CanChat (InGame g)
+instance CanChat GameOver
+
+checkPingTile :: Command (InQueue TwoVsTwo) -> Cmd.PingTile
+checkPingTile (PingTile pt) = pt
+checkPingTile (PingTile pt) = pt
