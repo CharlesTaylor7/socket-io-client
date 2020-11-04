98deb9c57344c61a181926c18b46533c48e7260a
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-11-03 23:42:35 -0500

    Work

--- generals-io-client/GeneralsIO/Strategy.hs
+++ generals-io-client/GeneralsIO/Strategy.hs
@@ -12,9 +12,6 @@ import Prelude hiding (print, putStrLn)
 import qualified Prelude
 
 import GHC.Generics (Generic)
-import Data.Functor (($>))
-import Control.Arrow ((|||))
-import Control.Monad (forever, when)
 import Control.Concurrent (ThreadId, forkIO)
 
 import Data.Generics.Labels ()
@@ -22,6 +19,7 @@ import Control.Lens
 import Data.Monoid (First(..))
 
 import Data.Text (Text)
+import qualified Data.Text.IO as T
 import qualified Data.UUID.V4 as UUID
 import Data.UUID (UUID)
 import qualified Data.UUID as UUID
@@ -33,8 +31,6 @@ import qualified Pipes.Prelude as Pipes
 
 import qualified SocketIO as Socket
 
-import GeneralsIO.Events
-import GeneralsIO.Commands
 
 
 type Strategy m =
@@ -68,7 +64,7 @@ playPrivateGame gameConfig bot = do
   let gameSize = gameConfig ^. #gameSize
   let botId = bot ^. #botId
   sendCommand $ JoinPrivate {..}
-  putStrLn $ "http://bot.generals.io/games/" <> gameId
+  liftIO $ T.putStrLn $ "http://bot.generals.io/games/" <> gameId
 
   let startGame q = (not $ q ^. #isForcing) && q ^. #numPlayers == gameSize
   q <- matchFirst (#_QueueUpdate . filtered startGame)
