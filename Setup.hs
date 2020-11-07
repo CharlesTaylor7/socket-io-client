46d6181a45352f563d2dd5fae659fbc7e697f5ae
CharlesTaylor7 <charlestaylor95@gmail.com>
2020-11-06 20:12:41 -0500

    WIP

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,21 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo
+import Distribution.PackageDescription
+import Distribution.Simple.Setup
+import Distribution.Simple.InstallDirs (absoluteInstallDirs)
+import Distribution.Simple.Setup
+
+import Distribution.Simple.Utils (installOrdinaryFiles, rawSystemExit)
+
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks { postBuild = installNodeDeps }
+
+
+installNodeDeps :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+installNodeDeps args flags desc info = do
+  -- let installDirs = absoluteInstallDirs _ _ _
+  -- dataDir = datadir installDirs
+  -- rawSystemExit "cd data_dir; yarn"
+  pure ()
+
